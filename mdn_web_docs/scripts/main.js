986df6c3c87c15a5ae664cf853a9f6c4fac9eec5
Darshna <darshna.adam@gmail.com>
2024-10-04 14:00:05 +0530

    html and css mini projects

---
+++ mdn_web_docs/scripts/main.js
@@ -0,0 +1,6 @@
+const myheading = document.querySelector("h1");
+myheading.textContent = "Hello, World!";
+
+document.querySelector("html").addEventListener("click", function () {
+  alert("Ouch, Stop poking me!");
+});
